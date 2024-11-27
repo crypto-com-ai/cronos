38182a0d45e48a8879d4015d7e6eef52e1da3d1f
HuangYi <huang@crypto.com>
2024-11-27 10:05:42 +0800

    emit event

--- integration_tests/contracts/contracts/TestLLama.sol
+++ integration_tests/contracts/contracts/TestLLama.sol
@@ -4,10 +4,17 @@ pragma solidity ^0.8.4;
 import {ILLamaModule} from "./src/LLama.sol";
 
 contract TestLLama {
+    event Inference(
+        string prompt,
+        string response
+    );
+
     address constant llamaContract = 0x0000000000000000000000000000000000000067;
     ILLamaModule llama = ILLamaModule(llamaContract);
 
     function inference(string calldata prompt, int64 seed, int32 steps) public returns (string memory) {
-        return llama.inference(prompt, seed, steps);
+        string memory rsp = llama.inference(prompt, seed, steps);
+        emit Inference(prompt, rsp);
+        return rsp;
     }
 }
--- integration_tests/test_llama.py
+++ integration_tests/test_llama.py
@@ -1,3 +1,5 @@
+import eth_abi
+
 from .utils import ADDRS, CONTRACTS, deploy_contract, send_transaction
 
 
@@ -5,10 +7,30 @@ def test_call(cronos):
     w3 = cronos.w3
     addr = ADDRS["validator"]
     contract = deploy_contract(w3, CONTRACTS["TestLLama"])
-    prompt = ""
+    prompt = "say hello"
     seed = 2
     steps = 256
     data = {"from": addr, "gasPrice": w3.eth.gas_price, "gas": 600000}
     tx = contract.functions.inference(prompt, seed, steps).build_transaction(data)
     receipt = send_transaction(w3, tx)
     assert receipt.status == 1
+    assert len(receipt.logs) == 1
+
+    # decode the event
+    rspprompt, rsp = eth_abi.decode(("string", "string"), receipt.logs[0].data)
+    assert rspprompt == prompt
+    assert rsp == (
+        "say helloeymy and Mabel. She was sock. Suddenled. piram the child, "
+        "she was called out of adventure came over there. The little Because. Jim "
+        "croak filter back down the child stepped in the small legs she was aunt to "
+        "sign and she and around her eyes! icy her clung and saw the second.\nThe "
+        "corner of her name - had gone. She flever.\nTen way and she had become a "
+        "lotion'dled again. Symbol started feeling filled the two, even heard. recogn "
+        "reached for a bro welcome child with gates that she asked her head to the "
+        "headdled, the old mistake.\nOne knee forward.\nLittle feet called in return "
+        "again. LSpace. She was a piece of his name: her furryed: a wooden become the "
+        "Mouse to shower warmly, her heart.\nInodedded towards the mouse mock was a "
+        "disagreak.\nHer home.\nThe rossy hazoint a second, she was.\nNow, mesint "
+        "telling Coraed “His and Joe had come from top. The gifted heart and she "
+        "noticed the stroked heart, so many little child.\nMatt."
+    )
